18aec33dd0a096ff6f66f6a29fd7386649a5d90a
Fedhira Syaila <pedirasyaila@gmail.com>
2023-10-23 16:32:08 +0700

    fedhira syaila

---
+++ Pertemuan04/Praktikum/p4_1_1214028/lib/material_page.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+// class DetailScreen extends StatelessWidget {
+//   const DetailScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Column(children: <Widget>[
+//         const Text('Ranca Upas'),
+//       ]),
+//     );
+//   }
+// }
